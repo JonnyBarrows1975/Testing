771af69873fe447ba068beaf2c1adb51fe68d120
Jon C. Barrows <jon.barrows@chartersolutions.com>
2016-05-20 00:10:16 -0500

    Hello

--- TestingApplication/TestingApplication/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace TestingApplication.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- TestingApplication/TestingApplication/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestingApplication.WebApiApplication" Language="C#" %>
